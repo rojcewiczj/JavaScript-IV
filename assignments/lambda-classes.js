6eb5183e5c7624be3cbaeb0b5adfc8bca7acef72
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-07-25 14:32:02 -0500

    working on lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attr){
+        this.name = attr.name,
+        this.age =  attr.age,
+        this.location = attr.location,
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+    }
+}
